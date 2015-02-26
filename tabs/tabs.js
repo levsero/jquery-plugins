6ee808c33a1da3ed7e2e179e76e2d11522b7f7d4
= <=>
2015-02-26 11:45:59 -0500

    funky calling tabs

---
+++ tabs/tabs.js
@@ -0,0 +1,39 @@
+$.Tabs = function (el) {
+  this.$el = $(el);
+  this.$contentTabs = $(document).find("." + this.$el.attr("data-content-tabs"));
+  this.$activeTab = this.$contentTabs.find(".active");
+
+  this.$el.on("click", "a", this.clickTab.bind(this))
+};
+
+$.Tabs.prototype.clickTab = function (event) {
+  event.preventDefault();
+
+  this.$el.find(".active").removeClass("active");
+  var $currentActive = $(event.currentTarget)
+  $currentActive.addClass("active");
+
+  this.$activeTab.addClass("transitioning");
+
+  this.$activeTab.on("transitionend", function(){
+
+    this.$activeTab.removeClass("active");
+    this.$activeTab.removeClass("transitioning");
+
+    this.$activeTab = this.$contentTabs.find("" + $currentActive.attr("href"));
+    this.$activeTab.addClass("active transitioning");
+
+    this.$activeTab.off()
+
+    window.setTimeout(function(event){
+      this.$activeTab.removeClass("transitioning");
+    }.bind(this), 0)
+
+  }.bind(this))
+}
+
+$.fn.tabs = function () {
+  return this.each(function () {
+    new $.Tabs(this);
+  });
+};
